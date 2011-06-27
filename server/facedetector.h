f692f7f2a8ea5b5351b4c703860e0db334397d9e
Novak Max <Novak.Max@gmail.com>
2011-06-27 21:01:31 +0000

    removed extraneous code

--- server/facedetector.h
+++ server/facedetector.h
@@ -1,57 +1,59 @@
-/*#if !defined FACEDETECTOR
+#if !defined FACEDETECTOR
 #define FACEDETECTOR
-*/
+
 #include "cv.h"
 #include "highgui.h"
 
 class facedetector{
 
+private:
+	IplImage *output;
 
 public:
-/*
-	bool itIsInitialized(IplImage *image){
-		return output &&(output->width == image->width) && (output->height == image->height);
-	}
 
-	void initializeMe(IplImage *image){
-		CvHaarClassifierCascade *pCascade = 0;
-		CvMemStorage *pStorage = 0;
-		CvSeq *pFaceRectSeq;
-		int i;
-		IplImage *output;
+	facedetector() : output(0){
 	
-		pStorage = cvCreateMemStorage(0);
-		pCascade = (CvHaarClassifierCascade *)cvLoad
-		((OPENCV_ROOT"/data/haarcascades/haarcascade_frontalface_default.xml"),
-			0, 0, 0 );
-
-
-
-		cvReleaseImage(&output);
-		output= cvCreateImage(cvSize(image->width,image->height),
-									image->depth, image->nChannels);
-	  }*/
-
-	IplImage process(IplImage *image){
-		pFaceRectSeq = cvHaarDetectObjects(image,pCascade, pStorage, 1.1, 3, CV_HAAR_DO_CANNY_PRUNING, cvSize(0,0));
-		for (i=0; i<(pFaceRectSeq? pFaceRectSeq->total:0);i++)
-		{
-			CvRect *r=(CvRect*)cvGetSeqElem(pFaceRectSeq,i);
-			CvPoint pt1 = {r->x, r->y};
-			CvPoint pt2 = {r->x + r-width, r->y + r->height};
-			cvRectangle(image, pt1, pt2, CV_RGB(0,225,0),3,4,0);
-			return image;
 	}
 
-	/*inline IplImage processImage(IplImage *image){
-		if (!itIsInitialized(image)){
-			initializeMe(image);
+	static void detect_and_draw(IplImage* img ){
+		//create the output file
+		IplImage* procImage;
+		procImage = cvCreateImage(cvSize(800,600),
+			img->depth,3);
+		//cvResize(img,procImage,CV_INTER_LINEAR);
+
+		CvMemStorage* storage = cvCreateMemStorage(0);
+		// Note that you must copy C:\Program Files\OpenCV\data\haarcascades\haarcascade_frontalface_alt2.xml
+		// to your working directory
+		CvHaarClassifierCascade* cascade = (CvHaarClassifierCascade*)cvLoad( "haarcascade_frontalface_alt2.xml" );
+		double scale = 1.3;
+
+		// Detect objects
+		cvClearMemStorage( storage );
+		CvSeq* objects = cvHaarDetectObjects( img, cascade, storage, 1.1, 4, CV_HAAR_DO_CANNY_PRUNING, cvSize(0, 0));
+
+		CvRect* r;
+		// Loop through objects and draw boxes
+		for( int i = 0; i < (objects ? objects->total : 0 ); i++ )
+		{
+			r = ( CvRect* )cvGetSeqElem( objects, i );
+			CvPoint pt1 = {r->x, r->y};
+			CvPoint pt2 = {r->x + r->width, r->y + r->height};
+			cvRectangle(img, pt1, pt2,CV_RGB(0,255,0),5,4,0);
+			//cvRectangle( img, cvPoint( r->x, r->y ), cvPoint( r->x + r->width, r->y + r->height ),
+			//	colors[i%8]);
 		}
-		image = process(image);
-		return image;
-	}*/
-/*
-	~facedetector(){
-		release();
-	}*/
+		std::cout<<"faces found"<<std::endl;
+		cvNamedWindow( "Output", CV_WINDOW_AUTOSIZE);
+		std::cout<<"resizing"<<std::endl;
+		//procImage=cvCloneImage(img);
+		cvResize(img,procImage,CV_INTER_NN);
+		//cvSaveImage("ProcImage",procImage);
+		cvShowImage( "Output", procImage);
+		cvWaitKey();
+
+		cvReleaseImage( &img );
+	}
+
 };
+#endif
